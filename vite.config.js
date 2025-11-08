5e1e086f413aa30819cfe950aa4ee9d5914ccd77
MBODM <webmaster@mbodm.com>
2025-11-08 12:11:10 +0100

    Fixed Firefox startup delay issue. Added linting to build script.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // The "server" and "preview" settings fix a Firefox startup delay issue
+  server: {
+    host: '0.0.0.0'
+  },
+  preview: {
+    host: '0.0.0.0'
+  }
 })
